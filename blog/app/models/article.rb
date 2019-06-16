55c3d0309bca457d51ee597ff2bcbcc7713e635f
yongcong yu <cleverforever@gmail.com>
2019-06-16 23:54:54 +0900

    add comment

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,6 +2,7 @@
 
 class Article < ApplicationRecord
   # ensure that all articles have a title that is at least five characters long
+  has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'welcome/ins'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#ins'
 end
